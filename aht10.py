cb71e8850d2760bfac58a8b5f6890780e2abf73d
クリスタル <xrystalll.io@gmail.com>
2024-06-15 15:50:26 +0300

    Fix aht10 lib bug with setting mode

--- aht10.py
+++ aht10.py
@@ -1,5 +1,4 @@
 import time
-from math import log
 
 AHT10_ADDRESS = 0x38
 AHT10_READ_DELAY_MS = 75
@@ -15,7 +14,7 @@ class AHT10:
     def __init__(self, i2c, mode=0, address=AHT10_ADDRESS):
         if i2c is None:
             raise ValueError('I2C object required.')
-        if mode is not (0 and 1):
+        if not mode in [0, 1]:
             raise ValueError('Mode must be either 0 for Celsius or 1 Farenheit')
         self.i2c = i2c
         self.address = address
@@ -26,7 +25,7 @@ class AHT10:
 
     def read_raw(self):
         self.i2c.writeto(self.address, CMD_MEASURE)
-        time.sleep_ms(AHT10_READ_DELAY_MS)
+        time.sleep_ms(AHT10_READ_DELAY_MS) # type: ignore
         self.readings_raw = self.i2c.readfrom(AHT10_ADDRESS, 6)
         self.results_parsed[0] = self.readings_raw[1] << 12 | self.readings_raw[2] << 4 | self.readings_raw[3] >> 4
         self.results_parsed[1] = (self.readings_raw[3] & 0x0F) << 16 | self.readings_raw[4] << 8 | self.readings_raw[5]
@@ -43,16 +42,9 @@ class AHT10:
             return ((self.results_parsed[1] / KILOBYTE_CONST) * AHT_TEMPERATURE_CONST - AHT_TEMPERATURE_OFFSET) * FARENHEIT_MULTIPLIER + FARENHEIT_OFFSET
 
     def set_mode(self, mode):
-        if mode is not (0 or 1):
+        if not mode in [0, 1]:
             raise ValueError('Mode must be either 0 for Celsius or 1 Farenheit')
         self.mode = mode
 
     def print(self):
         print("Temperature: " + str(self.temperature()) + ("C","F")[self.mode] + ", Humidity: " + str(self.humidity()))
-
-    def dew_point(self):
-        h = self.humidity()
-        prev_mode = self.mode
-        self.mode = 0
-        h = (log(h, 10) - 2) / 0.4343 + (17.62 * t) / (243.12 + t)
-        return 243.12 * h / (17.62 - h)
